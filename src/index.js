51607f84bb22ac6df126f8e0b82c4994f1a62b52
Kopyl10 <kopyl10@gmail.com>
2025-07-31 23:28:22 -0700

    kjkg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from './redux/store';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    {' '}
+    <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
